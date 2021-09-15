df5d2858a9ec365574fff11b0dc197f6db1c6e77
Effolex <Effolex@gmail.com>
2021-09-15 15:47:09 -0300

    Update exercise08.js

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/05-spread-operator-rest-destructuring-and-more/exercise08.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/05-spread-operator-rest-destructuring-and-more/exercise08.js
@@ -9,3 +9,21 @@ function greet(name,greeting = 'Hi') {
 assert.strictEqual(greet('John'), 'Hi John');
 assert.strictEqual(greet('John', 'Good morning'), 'Good morning John');
 assert.strictEqual(greet('Isabela', 'Oi'), 'Oi Isabela');
+
+const newUser = (id, name, email) => {
+  return {
+    id,
+    name,
+    email,
+  };
+};
+
+Class newUser {
+  construtor(id,name,email) {
+    this.id = id;
+    this.name = 
+  }
+}
+
+
+console.log(newUser(54, 'isabella', 'isabella@email.com')); // { id: 54, name: 'isabella', email: 'isabella@email.com' }
