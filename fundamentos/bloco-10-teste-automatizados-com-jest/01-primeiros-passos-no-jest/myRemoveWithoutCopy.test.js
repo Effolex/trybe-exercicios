58c5a50ad5ef276d46d63ba546abe9de9261f5e5
Eric Cruz <effolex@gmail.com>
2021-09-21 17:14:42 -0300

    dia-01-concluido a primeira parte, 5 testes que haviam sido feitos com assert, refeitos com jest

---
+++ fundamentos/bloco-10-teste-automatizados-com-jest/01-primeiros-passos-no-jest/myRemoveWithoutCopy.test.js
@@ -0,0 +1,18 @@
+const myRemoveWithoutCopy = require('./myRemoveWithoutCopy');
+
+describe('A função myRemoveWithoutCopy(arr, item) recebe um array arr e retorna o próprio array sem o elemento item caso ele exista no array',() => {
+  it('Verifique se a chamada myRemoveWithoutCopy([1, 2, 3, 4], 3) retorna o array esperado', () => {
+    expect(myRemoveWithoutCopy([1, 2, 3, 4], 3)).toEqual([1, 2, 4])
+  });
+  it('Verifique se a chamada myRemoveWithoutCopy([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]', () => {
+    expect(myRemoveWithoutCopy([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4])
+  });
+  it('Faça uma chamada para a função myRemoveWithoutCopy e verifique se o array passado por parâmetro sofreu alterações', () => {
+    const arr = [1,2,3,4];
+    myRemoveWithoutCopy(arr, 1)
+    expect(arr).toEqual([2, 3, 4])
+  });
+  it('Verifique se a chamada myRemoveWithoutCopy([1, 2, 3, 4], 5) retorna o array esperado', () => {
+    expect(myRemoveWithoutCopy([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4])
+  });
+});
