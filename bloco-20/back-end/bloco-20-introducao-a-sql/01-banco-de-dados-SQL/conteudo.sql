1c84568e74c3b38f701ce96e328e75e5fc9f4eb9
Effolex <Effolex@gmail.com>
2022-01-25 16:32:55 -0300

    Terceiro requisito

--- bloco-20/back-end/bloco-20-introducao-a-sql/01-banco-de-dados-SQL/conteudo.sql
+++ bloco-20/back-end/bloco-20-introducao-a-sql/01-banco-de-dados-SQL/conteudo.sql
@@ -4,6 +4,9 @@ SELECT * FROM sakila.film;
 # Escreva uma query que exiba apenas o nome dos filmes, seu ano de lançamento e sua classificação .
 SELECT title, release_year, rating FROM sakila.film;
 
+# Quantos filmes temos cadastrados?
+SELECT COUNT(*) FROM sakila.film;
+
 
 
 
