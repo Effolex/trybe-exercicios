fcbc015b872efd67de599a6bdc316180a2f3e862
Effolex <Effolex@gmail.com>
2022-01-25 16:32:47 -0300

    Segundo requisito

--- bloco-20/back-end/bloco-20-introducao-a-sql/01-banco-de-dados-SQL/conteudo.sql
+++ bloco-20/back-end/bloco-20-introducao-a-sql/01-banco-de-dados-SQL/conteudo.sql
@@ -1,6 +1,8 @@
 # Escreva uma query que exiba todos os filmes cadastrados no banco de dados.
 SELECT * FROM sakila.film;
 
+# Escreva uma query que exiba apenas o nome dos filmes, seu ano de lançamento e sua classificação .
+SELECT title, release_year, rating FROM sakila.film;
 
 
 
