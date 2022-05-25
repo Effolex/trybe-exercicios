0cf5d403ac218366664058ed9f4ba9bbf1769069
Effolex <effolex@gmail.com>
2022-05-24 22:45:24 -0300

    feat(30.2): exercise 4
    create the abstract class for mongoModel

---
+++ back-end/bloco-30 - MongoDB com Node.js e POO/MongoDB e POO/src/models/MongoModel.ts
@@ -0,0 +1,23 @@
+import { Model as M, Document, UpdateWriteOpResult, modelNames } from 'mongoose';
+import Model from '.';
+import { DeleteResult } from 'mongodb';
+
+abstract class MongoModel<T> implements Model<T> {
+  constructor(protected model: M<T & Document>) {}
+
+  create = async (obj: T):Promise<T> => this.model.create({ ...obj });
+
+  read = async (): Promise<T[]> => this.model.find();
+
+  readOne = async (id: string):Promise<T | null> =>
+    this.model.findOne({ id });
+  
+  update = async (dataToInsert: T, id: string):Promise<UpdateWriteOpResult> =>
+    this.model.updateOne({ _id: id }, { $set: dataToInsert });
+
+  delete = async (id: string):Promise<DeleteResult> =>
+    await this.model.deleteOne({ id });
+  
+}
+
+export default modelNames;
