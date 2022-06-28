54d2ea894d87cd9d77e31c43cf691969629e9b41
Effolex <effolex@gmail.com>
2022-06-28 15:31:09 -0300

    feat(sc): day one exercise 3

--- computer-science/bloco 33 - Introducao a Python/test.py
+++ computer-science/bloco 33 - Introducao a Python/test.py
@@ -17,3 +17,15 @@ def arithMean(list):
   for number in list:
     sum += number
   return sum / len(list)
+
+'''
+  Exercício 3:
+  Faça um programa que, dado um valor n qualquer,
+  tal que n > 1, imprima na tela um quadrado feito
+  de asteriscos de lado de tamanho n.
+'''
+
+def printSquare(size):
+  arr = [ '*' for i in range(size)]
+  for i in range(size):
+    print(''.join(arr))
