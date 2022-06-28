8c337a128e13ac162b945ade1c486da5816e40fc
Effolex <effolex@gmail.com>
2022-06-28 15:30:34 -0300

    feat(sc): day one exercise 2

--- computer-science/bloco 33 - Introducao a Python/test.py
+++ computer-science/bloco 33 - Introducao a Python/test.py
@@ -6,3 +6,14 @@
 
 def biggestN(x, y):
   return (x>y)*x + (y>x)*y;
+
+'''
+  🚀 Exercício 2:
+    Calcule a média aritmética dos valores contidos em uma lista.
+'''
+
+def arithMean(list):
+  sum = 0
+  for number in list:
+    sum += number
+  return sum / len(list)
